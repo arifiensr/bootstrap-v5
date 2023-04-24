e565ce969339d22c9e3144e14da76a3d85ef795e
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-04-24 17:28:37 +0700

    Progress navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'boxicons/css/boxicons.min.css'
+import './assets/css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
